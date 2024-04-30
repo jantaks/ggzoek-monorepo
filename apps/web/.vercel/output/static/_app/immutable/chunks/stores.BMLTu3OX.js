86d0adf601f5a40f19f03d5a5b77eee2fc7533a3
jantaks <jan.taks@gmail.com>
2024-04-30 15:19:27 +0200

    testing monorepo

--- apps/web/.vercel/output/static/_app/immutable/chunks/stores.BMLTu3OX.js
+++
@@ -1,1 +0,0 @@
-import{w as o}from"./index.DB2C4yXX.js";const e=o("",()=>(console.log("got a subscriber"),()=>console.log("no more subscribers")));export{e};
--- apps/web/.vercel/output/static/_app/immutable/entry/start.CAz2KRJa.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.D-YnbLQr.js";export{t as start};
