b2867f865022b6f3ccd8f080bdd2ee2b5ccbd7b8
jantaks <jan.taks@gmail.com>
2024-06-17 19:14:23 +0200

    Filters with filterstore

---
+++ apps/svelte5/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Searchform from '$lib/components/searchform/Searchform.svelte';
+
+  let { data } = $props();
+
+</script>
+
+<Searchform searchResponse={data.searchResponse}></Searchform>
