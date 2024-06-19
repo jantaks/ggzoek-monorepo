e70362958cb6940d58ec78ab42ebfafb32aa3eb5
jantaks <jan.taks@gmail.com>
2024-06-19 12:47:02 +0200

    Design stuff

--- apps/svelte5/src/lib/components/vacature-card/VacatureCard.svelte
+++ apps/svelte5/src/lib/components/vacature-card/VacatureCard.svelte
@@ -13,27 +13,36 @@
 
 </script>
 
+<div class="p-4 m-4 rounded bg-pink-50 text-slate-700">
+  <div class="bg-pink-50">
+    <h2 class="font-bold uppercase text-pink-500">{@html hit.title}</h2>
+  </div>
+  <div class="py-4">
+    <Tabs.Root>
+      <Tabs.List
+        class="lg:w-fit flex flex-row">
+        <Tabs.Trigger
+          value="overzicht">
+          <div class="py-0.5 px-4">overzicht</div>
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="kenmerken">
+          <div class="py-0.5 px-4">kenmerken</div>
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="overzicht">
+        <div class="py-4 px-4 bg-white shadow">
+          <p>{@html hit.summary}</p>
+        </div>
 
-<div class="p-4 m-4 border-2">
-  <Tabs.Root>
-    <Tabs.List
-      class="lg:w-fit flex flex-row gap-4  p-2">
-      <Tabs.Trigger
-        class="tab"
-        value="overzicht">overzicht
-      </Tabs.Trigger>
-      <Tabs.Trigger
-        class="tab"
-        value="kenmerken">kenmerken
-      </Tabs.Trigger>
-    </Tabs.List>
-    <Tabs.Content value="overzicht">
-      <h2>Title: {@html hit.title}</h2>
-      <p>{@html hit.summary}</p>
-    </Tabs.Content>
-    <Tabs.Content value="kenmerken">
-      Inhoud 2
-    </Tabs.Content>
-  </Tabs.Root>
+      </Tabs.Content>
+      <Tabs.Content value="kenmerken">
+        <div class="py-4 px-4 bg-white shadow">
+          <p>{@html hit.locaties}</p>
+        </div>
+      </Tabs.Content>
+    </Tabs.Root>
+  </div>
+
 
 </div>
--- apps/svelte5/src/routes/+page.svelte
+++ apps/svelte5/src/routes/+page.svelte
@@ -9,26 +9,18 @@
 
 
 </script>
-<style lang="postcss">
-    :global(.tab[data-state='active']) {
-        @apply bg-blue-500 text-white;
-    }
 
-    :global(.tab[data-state='inactive']) {
-        @apply bg-red-500 text-white;
-    }
-</style>
 
 <NavBar></NavBar>
 <div class="flex flex-row">
-  <div class="w-1/5 min-w-fit">
+  <div class="w-2/5 min-w-fit">
     <Searchform searchResponse={data.searchResponse}></Searchform>
   </div>
   <div class="w-full">
     <FilterBar />
     <!--    SEARCHRESULTS-->
 
-    <div class="bg-pink-400 p-4">
+    <div class="p-4">
       {#each data.searchResponse.hits as hit}
         <VacatureCard hit={hit}></VacatureCard>
       {/each}
