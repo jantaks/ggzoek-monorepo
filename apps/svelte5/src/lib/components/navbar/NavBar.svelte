5cc110a6af42c507b1ccc7298971d7ee51563f77
jantaks <jan.taks@gmail.com>
2024-08-02 12:20:58 +0200

    Adds about page

--- apps/svelte5/src/lib/components/navbar/NavBar.svelte
+++ apps/svelte5/src/lib/components/navbar/NavBar.svelte
@@ -3,26 +3,26 @@
   import { page } from '$app/stores';
 
   let { class: className } = $props();
-  let loggedIn = $derived.by(() => {
-    return !!$page.data.email;
-  });
+	let loggedIn = $derived.by(() => {
+		return !!$page.data.email;
+	});
 
 
 </script>
 
 <div class={"w-full " + className}>
-  <nav class={"flex items-center py-2 mx-auto max-w-7xl justify-between"}>
-    <h1 class="text-2xl text-pink-600 font-bold px-2 border-b-1">GGZOEK</h1>
-    <ul class="flex flex-row gap-8 mx-auto items-center">
-      <li><a href="/">Home</a></li>
-      <li><a href="/">Over ons</a></li>
-      <li><a href="/">Instellingen</a></li>
-      <li><a href="/">Professionals</a></li>
-    </ul>
-    {#if loggedIn}
-      <UserDropDown class=""></UserDropDown>
-    {:else}
-      <a href="/auth/login" class="text-pink-600">Login</a>
-    {/if}
-  </nav>
+	<nav class={"flex items-center py-2 mx-auto max-w-7xl justify-between"}>
+		<h1 class="text-2xl text-pink-600 font-bold px-2 border-b-1">GGZOEK</h1>
+		<ul class="flex flex-row gap-8 mx-auto items-center">
+			<li><a href="/">Home</a></li>
+			<li><a href="/pages/about">Over ons</a></li>
+			<li><a href="/">Instellingen</a></li>
+			<li><a href="/">Professionals</a></li>
+		</ul>
+		{#if loggedIn}
+			<UserDropDown class=""></UserDropDown>
+		{:else}
+			<a href="/auth/login" class="text-pink-600">Login</a>
+		{/if}
+	</nav>
 </div>
