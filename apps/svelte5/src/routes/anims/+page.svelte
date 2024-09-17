d8795c06a0d9bb7795730f1fb67c073f454ec52a
jantaks <jan.taks@gmail.com>
2024-09-17 19:21:19 +0200

    Scroll animation experimentation

--- apps/svelte5/src/routes/anims/+page.svelte
+++ apps/svelte5/src/routes/anims/+page.svelte
@@ -14,17 +14,6 @@
 
 	let form = getSearchForm();
 
-	// let kpis = [
-	// 	{ title: 'Verpleegkundigen', number: 347 },
-	// 	{ title: 'Psychiaters', number: 900 },
-	// 	{ title: 'GZ-psychologen', number: 1200 },
-	// 	{ title: 'Orthopedagogen', number: 500 },
-	// 	{ title: 'Sociaal psychiatrisch verpleegkundigen', number: 200 },
-	// 	{ title: 'Klinisch psychologen', number: 100 }
-	// ];
-
-	// $inspect(data.facets.beroepen);
-
 	let kpis = $derived.by(() => {
 		return data.facets.beroepen.map((beroep) => {
 			return { title: beroep.value, number: beroep.count };
@@ -38,52 +27,24 @@
 
 		gsap.registerPlugin(ScrollTrigger);
 
-		const tl = gsap.timeline()
+		gsap.timeline()
 			.from('#box-1', { x: '100%', duration: 1 }, 'start');
 
-		tl.tweenFromTo(0, 1);
 
-		// ScrollTrigger.create({
-		// 	trigger: '#text-1',
-		// 	start: 'top 10%%',
-		// 	animation: tl.resume(),
-		// 	onEnter: () => tl.reverse()
-		// });
+		let tl = gsap.timeline();
 
-		gsap.from('#box-2',
-			{
-				scrollTrigger: {
-					trigger: '#box-2',
-					start: 'top 80%',
-					end: 'top 20%',
-					toggleActions: 'restart reverse restart reverse',
-					scrub: 1
-					// pinSpacer: false,
-					// pin: true
-				},
-				height: 0,
-				width: 0,
-				opacity: 0,
-				duration: 1,
-				onUpdate: () => {
-					ScrollTrigger.refresh();
-				}
-			});
-
-
-		let tl2 = gsap.timeline();
-
-		tl2
+		tl
 			.to('#section-1', {
-				opacity: 0
+				opacity: 0,
+				duration: 0.5
 			})
 			.to('#box-3', {
 				opacity: '1',
 				backgroundColor: '#3E4665',
-				stagger: { amount: 1, from: 'random' },
-				duration: 0.2,
+				stagger: { amount: 2, from: 'random' },
+				duration: 0.5,
 				fontSize: '100%'
-			}, '<')
+			})
 			.from('#storage-number', {
 				innerText: 0,
 				stagger: { amount: 1, from: 'random' },
@@ -113,14 +74,11 @@
 			trigger: '#section-3',
 			start: 'top 30%',
 			end: 'top top',
-			toggleActions: 'restart none reverse reverse',
-			animation: tl2,
-			// scrub: 2,
+			toggleActions: 'restart none reset reset',
+			animation: tl,
 			pin: true,
 			pinSpacer: true,
 			markers: true
-			// toggleClass: { targets: '#box-3', className: 'hidden' }
-
 		});
 
 
@@ -132,43 +90,46 @@
 <svelte:head>
 	<style>
       body {
-          @apply bg-secondary-900;
+          @apply bg-primary-200;
           @apply text-primary-200;
       }
 	</style>
 </svelte:head>
-<section class="h-fit min-h-[60vh]" id="section-1">
-	<h1 class="py-10 text-6xl font-bold tracking-tight text-primary-200 text-center">Alle vacatures in de GGZ
-		<span
-			class=" text-primary">voor zorgprofessionals.</span></h1>
-	<div class="grid grid-cols-2 gap-10 py-8 ">
-		<div class="flex flex-col w-full max-w-xl gap-8" id="text-1">
-			<h2 class="text-3xl/10">Je hebt geen tijd om alle vacatures in de gaten te houden. Daarom doen wij het voor
-				je.</h2>
-			<p class="text-xl/8 text-primary-200 font-serif text-justify">
-				Voor professionals in de ggz is het moeilijk om een overzicht te krijgen van alle
-				vacatures. Daarom maken wij dagelijks een samenvatting van de vacatures van alle GGZ instellingen. We zoeken
-				naar de details die belangrijk zijn voor GGZ professionals en zetten deze voor jou op een rijtje. Wij zijn
-				gg<span class="">zoek</span>: het startpunt van jouw zoektocht naar een nieuwe baan in de GGZ.
-			</p>
-		</div>
-		{#if browser}
-			<div
-				class="h-fit glowing slide-in p-4 md:p-4 bg-gradient-to-r from-primary to-primary/90 md:rounded-xl shadow-2xl shadow-primary-400/70 text-primary-light space-y-4 w-full"
-				id="box-1"
-			>
-
-				<SearchBox />
-				<PostCodeSelect />
-				<FilterContainer facets={data.facets}></FilterContainer>
-				<ResultButton
-					href={`/zoekresultaten?${$page.url.searchParams}`}
-					isLoading={form.isLoading}
-					totalHits={data.searchResponse.estimatedTotalHits} />
+<div class="bg-secondary-900" id="section-1">
+	<section class="h-fit min-h-[60vh] bg-secondary-900">
+		<h1 class="py-10 text-6xl font-bold tracking-tight text-primary-200 text-center">Alle vacatures in de GGZ
+			<span
+				class=" text-primary">voor zorgprofessionals.</span></h1>
+		<div class="grid grid-cols-2 gap-10 py-8 ">
+			<div class="flex flex-col w-full max-w-xl gap-8" id="text-1">
+				<h2 class="text-3xl/10">Je hebt geen tijd om alle vacatures in de gaten te houden. Daarom doen wij het voor
+					je.</h2>
+				<p class="text-xl/8 text-primary-200 font-serif text-justify">
+					Voor professionals in de ggz is het moeilijk om een overzicht te krijgen van alle
+					vacatures. Daarom maken wij dagelijks een samenvatting van de vacatures van alle GGZ instellingen. We zoeken
+					naar de details die belangrijk zijn voor GGZ professionals en zetten deze voor jou op een rijtje. Wij zijn
+					gg<span class="">zoek</span>: het startpunt van jouw zoektocht naar een nieuwe baan in de GGZ.
+				</p>
 			</div>
-		{/if}
-	</div>
-</section>
+			{#if browser}
+				<div
+					class="h-fit glowing slide-in p-4 md:p-4 bg-gradient-to-r from-primary to-primary/90 md:rounded-xl shadow-2xl shadow-primary-400/70 text-primary-light space-y-4 w-full"
+					id="box-1"
+				>
+
+					<SearchBox />
+					<PostCodeSelect />
+					<FilterContainer facets={data.facets}></FilterContainer>
+					<ResultButton
+						href={`/zoekresultaten?${$page.url.searchParams}`}
+						isLoading={form.isLoading}
+						totalHits={data.searchResponse.estimatedTotalHits} />
+				</div>
+			{/if}
+		</div>
+	</section>
+</div>
+
 
 {#snippet kpi(title, number)}
 
@@ -195,7 +156,8 @@
 			<div class="flex flex-col w-full max-w-xl gap-8 text-secondary-900" id="text-1">
 				<h2 class="text-3xl/10">Iedereen op de juiste plek. Dat is onze missie. </h2>
 				<p class="text-xl/8  font-serif text-justify">
-					De GGZ kampt met een groot capaciteitsprobleem. Veel professionals kampen met een hoge werkdruk. Dit gaat ten
+					De GGZ kampt met een groot capaciteitsprobleem. Veel professionals ervaren een (te) hoge werkdruk. Dit gaat
+					ten
 					koste van
 					werkplezier, kwaliteit van zorg en de gezondheid van de professional. Uiteindelijk komt dit ook de
 					productiviteit niet ten goede. Door er voor te zorgen
@@ -209,14 +171,16 @@
 	</section>
 </div>
 <div class="bg-secondary-900	 h-full">
-	<section class="text-white w-full flex flex-row justify-between items-center" id="section-2">
+	<section class="text-white w-full flex flex-row justify-between " id="section-2">
 		<div class="flex flex-col w-full max-w-xl gap-4" id="text-1">
 			<h2 class="text-3xl/10">Toch niet gevonden wat je zoekt?</h2>
-			<p class="text-xl/8 text-primary-200 font-serif text-justify">
+			<p class="text-xl/8 text-primary-200 font-serif text-justify" id="uitdagingen">
 				GGZoek gaat graag met je in gesprek over jouw wensen en uitdagingen.
 			</p>
 		</div>
-		<div class=" w-[600px] h-[800px] bg-pink-400" id="box-2"></div>
+		<div class="h-full w-full flex flex-col items-center justify-center" id="box-2">
+			<img alt="Jan Taks" id="img-1" src="/boef.png" />
+		</div>
 	</section>
 </div>
 
